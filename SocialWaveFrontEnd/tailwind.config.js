adebb01ab0e69ee1bd68621a7011055111d65ba3
Lucsff <lucas.fernandes.13@hotmail.com>
2023-11-22 18:54:20 -0300

    some changes on Layout

---
+++ SocialWaveFrontEnd/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-purple": "#081A51",
+        "light-white": "rgba(255,255,255,0.17)",
+      }
+    },
+  },
+  plugins: [],
+}
+
