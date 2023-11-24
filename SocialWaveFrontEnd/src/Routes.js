64a06a96f959bed787e4e94e8cbb2e747fa64c2b
Lucas-Teixeira <lucas.s.teixeira@viannasempre.com.br>
2023-11-24 14:10:22 -0300

    Iniciando mudanças

--- SocialWaveFrontEnd/src/Routes.js
+++ SocialWaveFrontEnd/src/Routes.js
@@ -1,18 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./screens/Layout/Layout";
-import Home from "./screens/Home/Home";
-import Login from './screens/Login/Login';
-import SignUp from './screens/SignUp/SignUp';
+import Home from "./pages/Home";
 
 const Rotas = () => {
     return (
     <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />}>
             <Route index element={<Home />} />
-            <Route path='login' element={<Login />}/>
-            <Route path='signup' element={<SignUp />}/>
           </Route>
         </Routes>
   </BrowserRouter>
--- SocialWaveFrontEnd/src/components/Sidebar.js
+++ SocialWaveFrontEnd/src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import {
     Flex,
     Text,
@@ -19,62 +20,64 @@ import {
 import NavItem from '../components/NavItem'
 
 export default function Sidebar() {
-    const [navSize, changeNavSize] = useState("large")
-    return (
-        <Flex
-            pos="sticky"
-            left="5"
-            h="95vh"
-            marginTop="2.5vh"
-            boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-            borderRadius={navSize == "small" ? "15px" : "30px"}
-            w={navSize == "small" ? "75px" : "200px"}
-            flexDir="column"
-            justifyContent="space-between"
-        >
-            <Flex
-                p="5%"
-                flexDir="column"
-                w="100%"
-                alignItems={navSize == "small" ? "center" : "flex-start"}
-                as="nav"
-            >
-                <IconButton
-                    background="none"
-                    mt={5}
-                    _hover={{ background: 'none' }}
-                    icon={<FiMenu />}
-                    onClick={() => {
-                        if (navSize == "small")
-                            changeNavSize("large")
-                        else
-                            changeNavSize("small")
-                    }}
-                />
-                <NavItem navSize={navSize} icon={FiHome} title="Dashboard" description="This is the description for the dashboard." />
-                <NavItem navSize={navSize} icon={FiCalendar} title="Calendar" active />
-                <NavItem navSize={navSize} icon={FiUser} title="Clients" />
-                <NavItem navSize={navSize} icon={FiDollarSign} title="Stocks" />
-                <NavItem navSize={navSize} icon={FiBriefcase} title="Reports" />
-                <NavItem navSize={navSize} icon={FiSettings} title="Settings" />
-            </Flex>
 
+    const [navSize, changeNavSize] = useState("large");
+    const navigate = useNavigate();
+
+    return (
+        <React.Fragment>        
             <Flex
-                p="5%"
+                pos="sticky"
+                left="5"
+                h="95vh"
+                marginTop="2.5vh"
+                boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
+                borderRadius={navSize == "small" ? "15px" : "30px"}
+                w={navSize == "small" ? "75px" : "200px"}
                 flexDir="column"
-                w="100%"
-                alignItems={navSize == "small" ? "center" : "flex-start"}
-                mb={4}
+                justifyContent="space-between"
             >
-                <Divider display={navSize == "small" ? "none" : "flex"} />
-                <Flex mt={4} align="center">
-                    <Avatar size="sm" src="avatar-1.jpg" />
-                    <Flex flexDir="column" ml={4} display={navSize == "small" ? "none" : "flex"}>
-                        <Heading as="h3" size="sm">Sylwia Weller</Heading>
-                        <Text color="gray">Admin</Text>
+                <Flex
+                    p="5%"
+                    flexDir="column"
+                    w="100%"
+                    alignItems={navSize == "small" ? "center" : "flex-start"}
+                    as="nav"
+                >
+                    <IconButton
+                        background="none"
+                        mt={5}
+                        _hover={{ background: 'none' }}
+                        icon={<FiMenu />}
+                        onClick={(title) => {
+                            navigate(`/${title}`);
+                        }}
+                    />
+                    <NavItem navSize={navSize} icon={FiHome} title="Dashboard" description="This is the description for the dashboard." />
+                    <NavItem navSize={navSize} icon={FiCalendar} title="Calendar" active />
+                    <NavItem navSize={navSize} icon={FiUser} title="Clients" />
+                    <NavItem navSize={navSize} icon={FiDollarSign} title="Stocks" />
+                    <NavItem navSize={navSize} icon={FiBriefcase} title="Reports" />
+                    <NavItem navSize={navSize} icon={FiSettings} title="Settings" />
+                </Flex>
+
+                <Flex
+                    p="5%"
+                    flexDir="column"
+                    w="100%"
+                    alignItems={navSize == "small" ? "center" : "flex-start"}
+                    mb={4}
+                >
+                    <Divider display={navSize == "small" ? "none" : "flex"} />
+                    <Flex mt={4} align="center">
+                        <Avatar size="sm" src="avatar-1.jpg" />
+                        <Flex flexDir="column" ml={4} display={navSize == "small" ? "none" : "flex"}>
+                            <Heading as="h3" size="sm">Sylwia Weller</Heading>
+                            <Text color="gray">Admin</Text>
+                        </Flex>
                     </Flex>
                 </Flex>
             </Flex>
-        </Flex>
+        </React.Fragment>
     )
 }
